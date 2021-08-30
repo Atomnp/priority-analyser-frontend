90139d3a79df051094ed8516c86fe9ead916adc0
aayush <neupane0403@gmail.com>
2021-08-30 22:31:48 +0545

    create react app wala message removed and Faq added to obile biew

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -4,12 +4,13 @@ import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import InsertChartIcon from "@material-ui/icons/InsertChart";
 import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+import FaqIcon from "@material-ui/icons/LiveHelp";
 
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '&$selected': {
+    "&$selected": {
       color: theme.palette.secondary.main,
     },
   },
@@ -22,18 +23,26 @@ export default function SimpleBottomNavigation() {
   const history = useHistory();
   return (
     <BottomNavigation
-    value={value}
-    onChange={(event, newValue) => {
+      value={value}
+      onChange={(event, newValue) => {
         setValue(newValue);
-        const path = newValue === 0 ? "/predict" : "/analyse";
+        let path = "";
+        if (newValue === 0) {
+          path = "/predict";
+        } else if (newValue === 1) {
+          path = "/analyse";
+        } else {
+          path = "/faq";
+        }
+        // const path = newValue === 0 ? "/predict" : "/analyse";
         history.push(path);
       }}
       showLabels
-      >
+    >
       <BottomNavigationAction
         classes={{
           root: classes.root,
-          selected: classes.selected
+          selected: classes.selected,
         }}
         showLabel={true}
         label="Predict"
@@ -42,11 +51,20 @@ export default function SimpleBottomNavigation() {
       <BottomNavigationAction
         classes={{
           root: classes.root,
-          selected: classes.selected
+          selected: classes.selected,
         }}
         showLabel={true}
         label="Analyse"
         icon={<InsertChartIcon />}
+      />
+      <BottomNavigationAction
+        classes={{
+          root: classes.root,
+          selected: classes.selected,
+        }}
+        showLabel={true}
+        label="Help"
+        icon={<FaqIcon />}
       />
     </BottomNavigation>
   );
--- src/faq/index.js
+++ src/faq/index.js
@@ -1,73 +1,80 @@
 import React from "react";
 import Faq from "react-faq-component";
-import {
-    lightBlue,
-    grey
-  } from "@material-ui/core/colors";
-import { useTheme } from '@material-ui/core/styles';
-import { PredictionTable, BasicTable } from './table.js';
+import { lightBlue, grey } from "@material-ui/core/colors";
+import { useTheme } from "@material-ui/core/styles";
+import { PredictionTable, BasicTable } from "./table.js";
 import Typography from "@material-ui/core/Typography";
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 
 const data = {
-    title: "FAQ",
-    rows: [
-        {
-            title: "How we predict the results?",
-            content:<div>
-                <p>First, we calculate the cutoff marks of each program, then remove the outliers and use the confidence interval to predict the result.</p>
-                <PredictionTable/>
-            </div>
-        },
-        {
-            title: "How does analysis page works?",
-            content: `It works on the basis of cutoff marks of each program and colleges. Here, we use range based bar graph to show cutoff marks of various programs and colleges.
+  title: "FAQ",
+  rows: [
+    {
+      title: "How we predict the results?",
+      content: (
+        <div>
+          <p>
+            First, we calculate the cutoff marks of each program, then remove
+            the outliers and use the confidence interval to predict the result.
+          </p>
+          <PredictionTable />
+        </div>
+      ),
+    },
+    {
+      title: "How does analysis page works?",
+      content: `It works on the basis of cutoff marks of each program and colleges. Here, we use range based bar graph to show cutoff marks of various programs and colleges.
             We also provide information about the number of seats available in the form of pie chart.`,
-        },
-        {
-            title: "Does our calulation includes the various quota system of IOE?",
-            content: `No, currently our system doesnot take quota system into consideration. We take them as outliers and do calulation accordingly.`,
-        },
-        {
-            title: "What are the Full Form of colleges?",
-            content: <div>
-                <Typography variant="body1">The list of all the colleges of IOE with their code name and full form is shown below</Typography>
-                <BasicTable college={true} />
-                </div>
-        },
-        {
-            title: "What are the Full Form of programs?",
-            content: <div>
-                <Typography variant="body1">The list of all the programs of IOE with their code name and full form is shown below</Typography>
-                <BasicTable college={false} />
-            </div>
-        },
-    ],
+    },
+    {
+      title: "Does our calulation includes the various quota system of IOE?",
+      content: `No, currently our system doesnot take quota system into consideration. We take them as outliers and do calulation accordingly.`,
+    },
+    {
+      title: "What are the Full Form of colleges?",
+      content: (
+        <div>
+          <Typography variant="body1">
+            The list of all the colleges of IOE with their code name and full
+            form is shown below
+          </Typography>
+          <BasicTable college={true} />
+        </div>
+      ),
+    },
+    {
+      title: "What are the Full Form of programs?",
+      content: (
+        <div>
+          <Typography variant="body1">
+            The list of all the programs of IOE with their code name and full
+            form is shown below
+          </Typography>
+          <BasicTable college={false} />
+        </div>
+      ),
+    },
+  ],
 };
 
+const FAQ = ({ setCurrentPage }) => {
+  setCurrentPage("faq");
+  const theme = useTheme();
 
-const FAQ = ({setCurrentPage}) => {
-    setCurrentPage("faq");
-    const theme = useTheme();
-
-    const styles = {
-        bgColor: theme.palette.background.default,
-        titleTextColor: theme.palette.text.primary,
-        rowTitleColor:  theme.palette.text.primary,
-        rowContentColor: theme.palette.text.secondary,
-        arrowColor: theme.palette.secondary.main,
-    };
-    return (
-        <div>
-            <Container maxWidth="sm">
-            <Faq
-                data={data}
-                styles={styles}
-                />
-            </Container>
-        </div>
-    );
+  const styles = {
+    bgColor: theme.palette.background.default,
+    titleTextColor: theme.palette.text.primary,
+    rowTitleColor: theme.palette.text.primary,
+    rowContentColor: theme.palette.text.secondary,
+    arrowColor: theme.palette.secondary.main,
+  };
+  return (
+    <div>
+      <Container style={{ paddingBottom: "3rem" }} maxWidth="sm">
+        <Faq data={data} styles={styles} />
+      </Container>
+    </div>
+  );
 };
 
 export default FAQ;
-
